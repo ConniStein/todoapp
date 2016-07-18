6d1b575c007095b5dafcdb6ed4b1fa2762b0699b
ConniStein <cvillagranpoblete1994@gmail.com>
2016-07-18 03:43:48 -0400

    hice funcionar el input aun no se como hacer check y eliminar

--- toDo.js
+++ toDo.js
@@ -2,3 +2,32 @@
 //AAAAAAAAAAAAAAAHHHHHHHHHHHHH
 //NO ENTIENDO
 
+(function(){
+
+  var nuevaTarea = document.querySelector( '#tareaNueva' ),
+      form = document.querySelector( 'form' ),
+      addTarea = document.querySelector( '#agregarTarea' );
+    
+  form.addEventListener( 'submit', function( blankSpace ) {
+    var text = addTarea.value;
+    if ( text !== '' ) {
+      nuevaTarea.innerHTML += '<li>' + text + '</li>';
+      addTarea.value = '';
+      addTarea.focus();
+    }
+    blankSpace.preventDefault();
+  }, false);
+
+  todo.addEventListener( 'click', function( blankSpace ) {
+    var tarea = blankSpace.target;
+    if ( tarea.tagName === 'LI' ) {
+      if ( tarea.classList.contains( 'done' ) ) {
+        tarea.parentNode.removeChild( t );
+      } else {  
+        tarea.classList.add( 'done' );
+      }
+    };
+    blankSpace.preventDefault();
+  }, false);
+
+})();
